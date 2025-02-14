784bc8921f948cc51207acd8b09a27250bebf4e9
unknown <dreicleal@hotmail.com>
2025-02-14 21:36:01 +0000

    Slide Component
    Slide component with React + tailwind

---
+++ slide/src/app/components/Slide.tsx
@@ -0,0 +1,63 @@
+"use client";
+import { useRef, useState } from "react";
+
+export default function Slide() {
+  const startX = useRef<number | null>(null);
+  const [slideLeft, setSlideLeft] = useState(false);
+  const [slideRight, setSlideRight] = useState(false);
+
+  const handleStart = (e: React.TouchEvent) => {
+    startX.current = e.touches[0].clientX;
+  };
+
+  const handleMove = (e: React.TouchEvent) => {
+    if (startX.current) {
+      const moveX = startX.current - e.touches[0].clientX;
+      if (moveX < -40) {
+        setSlideLeft(true);
+        setSlideRight(false);
+      }
+      if (slideLeft && moveX > 20) {
+        setSlideLeft(false);
+      }
+      if (slideRight && moveX < -20) {
+        setSlideRight(false);
+      }
+
+      if (moveX > 40) {
+        setSlideRight(true);
+        setSlideLeft(false);
+      }
+    }
+  };
+
+  return (
+    <div
+      onTouchStart={handleStart}
+      onTouchMove={handleMove}
+      className="flex items-center justify-center relative overflow-hidden rounded-md"
+    >
+      <div
+        className={`${
+          slideLeft && "translate-x-[0px]"
+        } p-4 absolute translate-x-[-70px] left-0 bg-green-500 w-[70px] text-center transition duration-300`}
+      >
+        Left
+      </div>
+      <div
+        className={`p-4 ${slideLeft && "translate-x-[70px]"} ${
+          slideRight && "translate-x-[-70px]"
+        } bg-slate-500 w-[240px] text-center transition duration-300`}
+      >
+        Center
+      </div>
+      <div
+        className={`${
+          slideRight && "translate-x-[0px]"
+        } p-4 right-0 absolute bg-blue-500 w-[70px] translate-x-[70px] text-center transition duration-300`}
+      >
+        Right
+      </div>
+    </div>
+  );
+}
